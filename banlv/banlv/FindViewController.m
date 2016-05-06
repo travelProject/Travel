20053208061c35068cbcfde3e8a1e84b72c9a587
lcy <lcy@lcydemacbook-air.local>
2016-05-06 11:42:19 +0800

    发现1.1

--- banlv/banlv/FindViewController.m
+++ banlv/banlv/FindViewController.m
@@ -24,6 +24,30 @@
     
     self.view.backgroundColor = [UIColor cyanColor];
     
+    UIButton *callBtn = [[UIButton alloc] init];
+    
+    callBtn.frame = CGRectMake(0, 44 + 10, kScreenFrameW, 200);
+    
+    callBtn.backgroundColor = [UIColor redColor];
+    
+    [self.view addSubview:callBtn];
+    
+    [callBtn addTarget:self action:@selector(call:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    
+    UIButton *partyBtn = [[UIButton alloc] init];
+    
+    partyBtn.frame = CGRectMake(0,44 + 210, kScreenFrameW, 200);
+    
+    partyBtn.backgroundColor = [UIColor blueColor];
+    
+    [self.view addSubview:partyBtn];
+    
+    [partyBtn addTarget:self action:@selector(party:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    
     
     
     
@@ -50,14 +74,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
