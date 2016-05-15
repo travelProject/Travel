b7fd5c7d76fc1dd550db349da63e185d4d9eeacd
gjxylcy <gjxylcy@163.com>
2016-05-15 18:35:22 +0800

    juhui1.1

---
+++ banlv/banlv/类别/发现/聚会/CYPartyCell.h
@@ -0,0 +1,14 @@
+//
+//  CYPartyCell.h
+//  banlv
+//
+//  Created by lcy on 16/5/15.
+//  Copyright © 2016年 llz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYPartyCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIView *bgImgView;
+
+@end
