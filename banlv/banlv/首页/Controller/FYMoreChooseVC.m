e199f0b4dd96d557522843bb0dea5a28b5483089
lfy <lfy@bob.local>
2016-05-21 17:52:53 +0800

    星期六

--- banlv/banlv/首页/Controller/FYMoreChooseVC.m
+++ banlv/banlv/首页/Controller/FYMoreChooseVC.m
@@ -10,14 +10,98 @@
 
 @interface FYMoreChooseVC ()
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *scrollViewH;
+@property (weak, nonatomic) IBOutlet UIView *choosePeopleNum;
+@property (weak, nonatomic) IBOutlet UIButton *searchBtn;
+@property (weak, nonatomic) IBOutlet UILabel *HostSex;
+@property (weak, nonatomic) IBOutlet UILabel *peopleNumLimmit;
+
+@property(nonatomic,strong)NSArray *sexArr;
+
+@property(nonatomic,assign) NSUInteger sexCount;
+
+@property(nonatomic,assign)NSInteger peopleCount;
 
 @end
 
 @implementation FYMoreChooseVC
 
+- (NSArray *)sexArr
+{
+    if (!_sexArr) {
+        
+        _sexArr = [NSArray arrayWithObjects:@"男", @"男女不限" ,@"女" ,nil];
+    }
+    
+    return _sexArr;
+}
+
+- (NSUInteger)sexCount
+{
+    if (!_sexCount) {
+        
+        _sexCount = 100;
+        
+    }
+    return _sexCount;
+}
+
+- (NSInteger)peopleCount
+{
+    if (!_peopleCount) {
+        
+        _peopleCount = 1;
+    }
+    return _peopleCount;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+}
+- (IBAction)chooseHouseLocation:(id)sender {
+    
+    NSLog(@"选择房源");
+    
+}
+- (IBAction)leftArrow:(id)sender {
+    
+    self.sexCount--;
+    
+    self.HostSex.text = self.sexArr[self.sexCount % 3];
+    
+}
+- (IBAction)rightArrow:(id)sender {
+    
+    self.sexCount += 1;
+    
+    self.HostSex.text = self.sexArr[self.sexCount % 3];
+ 
+}
+- (IBAction)divBtn:(id)sender {
+    
+    UIButton *divBtn = (UIButton *)sender;
+    
+    self.peopleCount--;
+    
+    self.peopleNumLimmit.text = [NSString stringWithFormat:@"%ld",self.peopleCount];
+    
+    if (self.peopleCount == 1) {
+        
+        divBtn.enabled = NO;
+    }
+    
+}
+- (IBAction)plusBtn:(id)sender {
+    
+    UIButton *divBtn = (UIButton *)sender;
+    self.peopleCount++;
+    self.peopleNumLimmit.text = [NSString stringWithFormat:@"%ld",self.peopleCount];
+    
+    if (self.peopleCount == 4) {
+        
+        divBtn.enabled = NO;
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,7 +113,7 @@
 {
     [super updateViewConstraints];
     
-    self.scrollViewH.constant = self.view.height;
+    self.scrollViewH.constant = CGRectGetMaxY(self.choosePeopleNum.frame) + self.searchBtn.height + 5;
 }
 
 @end
