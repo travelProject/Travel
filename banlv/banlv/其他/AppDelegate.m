eaa66c15e8a1623465e88cabf0c3a079801256ab
lfy <lfy@bob.local>
2016-05-28 12:34:58 +0800

    wu

--- banlv/banlv/其他/AppDelegate.m
+++ banlv/banlv/其他/AppDelegate.m
@@ -31,7 +31,7 @@
     // 要使用百度地图，请先启动BaiduMapManager 
     _mapManager = [[BMKMapManager alloc]init];
     
-    BOOL ret = [_mapManager start:@"DsmdGAOS6xQgjbuEXynqIT70jdnlGYvV"  generalDelegate:nil];
+    BOOL ret = [_mapManager start:@"rRHanAe9IfoeT4VpDFAEHSwCpGBVcMic"  generalDelegate:nil];
     if (!ret) {
         NSLog(@"manager start failed!");
     }
