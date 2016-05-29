0c75e976915a9d34c0fa97f5bbe18d089c0d0b6f
lifeiyang <lfy@lifeiyangs-imac.local>
2016-05-29 23:10:00 +0800

    首页/搜索模块基本完成

---
+++ banlv/banlv/首页/Controller/FYApplyBookingHouseVC.h
@@ -0,0 +1,15 @@
+//
+//  FYApplyBookingHouseVC.h
+//  banlv
+//
+//  Created by lifeiyang on 16/5/29.
+//  Copyright © 2016年 llz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FYApplyBookingHouseVC : BLBackViewController
+
+@property(nonatomic,copy) NSString *ownerName;
+
+@end
